8e40d859d1c004c1063a20f6b93075b41105818e
EdgarFav <edgarfda17@gmail.com>
2023-04-15 23:49:44 -0600

    adding play-pause button

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useRef } from 'react'
+import { FaPlay, FaPause } from 'react-icons/fa'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isPlay, setIsPlay] = useState(false)
+  const videoRef = useRef()
+  console.log("videoRef", videoRef);
+
+  function handlePlay() {
+    const video = videoRef.current
+    if (video.paused) {
+      setIsPlay(true)
+      video.play()
+    } else {
+      setIsPlay(false)
+      video.pause()
+    }
+  }
 
   return (
     <div className="App">
       <h1>VIDEO PLAYER</h1>
-      <video controls src="https://media.istockphoto.com/id/1070580916/es/v%C3%ADdeo/t-l-grupo-de-personas-caminando-en-las-escaleras-por-la-noche.mp4?s=mp4-640x640-is&k=20&c=2RixD3-bwJQQ345Ys4vXqCVwbyDZcvGH7IRUJV5pXAs="></video>
+      <video ref={videoRef} src="https://media.istockphoto.com/id/1070580916/es/v%C3%ADdeo/t-l-grupo-de-personas-caminando-en-las-escaleras-por-la-noche.mp4?s=mp4-640x640-is&k=20&c=2RixD3-bwJQQ345Ys4vXqCVwbyDZcvGH7IRUJV5pXAs="></video>
+      <div className='nav_controls'>
+        <div>
+          <button className='play_pause' onClick={handlePlay}>
+            {
+              isPlay ? <FaPause /> : <FaPlay />
+            }
+          </button>
+        </div>
+      </div>
     </div>
+
   )
 }
 
 export default App
+
+/*
+current -> name value play() pause () duration ()
+*/
