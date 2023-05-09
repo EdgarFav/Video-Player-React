7eb6ff9d3439f09cc9e6501da233e6a770bc50c9
EdgarFav <edgarfda17@gmail.com>
2023-05-08 23:38:16 -0600

    Feat: adding bottons forward and backward

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react'
-import { FaPlay, FaPause } from 'react-icons/fa'
+import { FaPlay, FaPause, FaBackward, FaForward } from 'react-icons/fa'
 import './App.css'
 
 function App() {
@@ -18,17 +18,33 @@ function App() {
     }
   }
 
+  function handleRewind() {
+    const video = videoRef.current
+    video.currentTime -= 10
+  }
+
+  function handleFast() {
+    const video = videoRef.current
+    video.currentTime += 10
+  }
+
   return (
-    <div className="App">
+    <div className="container">
       <h1>VIDEO PLAYER</h1>
-      <video ref={videoRef} src="https://media.istockphoto.com/id/1070580916/es/v%C3%ADdeo/t-l-grupo-de-personas-caminando-en-las-escaleras-por-la-noche.mp4?s=mp4-640x640-is&k=20&c=2RixD3-bwJQQ345Ys4vXqCVwbyDZcvGH7IRUJV5pXAs="></video>
+      <video ref={videoRef} src="https://media.istockphoto.com/id/127737092/es/v%C3%ADdeo/azul-paro-posici%C3%B3n-elevada-en-la-branch.mp4?s=mp4-640x640-is&k=20&c=87bYgDjA6NhGUts63Nt7lkdsF_nGW_aHKG2zu1SSlMg="></video>
       <div className='nav_controls'>
+        <div>
+          <button onClick={handleRewind}><FaBackward /></button>
+        </div>
         <div>
           <button className='play_pause' onClick={handlePlay}>
             {
               isPlay ? <FaPause /> : <FaPlay />
             }
           </button>
+        </div>
+        <div>
+          <button onClick={handleFast}><FaForward /></button>
         </div>
       </div>
     </div>
