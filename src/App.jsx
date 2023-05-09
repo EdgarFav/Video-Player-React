04dd1df4e7ae1b7062a2021a7be3ad4b94b44e98
EdgarFav <edgarfda17@gmail.com>
2023-05-09 00:07:49 -0600

    Feat: adding progress bar and duration plus current time

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import './App.css'
 
 function App() {
   const [isPlay, setIsPlay] = useState(false)
+  const [duration, setDuration] = useState(0)
+  const [currentTime, setCurrentTime] = useState(0)
   const videoRef = useRef()
-  console.log("videoRef", videoRef);
+  //console.log("videoRef", videoRef);
 
   function handlePlay() {
     const video = videoRef.current
@@ -28,10 +30,32 @@ function App() {
     video.currentTime += 10
   }
 
+  function handleTime() {
+    const video = videoRef.current
+    setCurrentTime(video.currentTime)
+    setDuration(video.duration)
+  }
+
+  function handleProgress(e) {
+    const video = videoRef.current
+    video.currentTime = e.target.value
+  }
+
   return (
     <div className="container">
       <h1>VIDEO PLAYER</h1>
-      <video ref={videoRef} src="https://media.istockphoto.com/id/127737092/es/v%C3%ADdeo/azul-paro-posici%C3%B3n-elevada-en-la-branch.mp4?s=mp4-640x640-is&k=20&c=87bYgDjA6NhGUts63Nt7lkdsF_nGW_aHKG2zu1SSlMg="></video>
+      <video
+        onTimeUpdate={handleTime}
+        ref={videoRef}
+        src="https://media.istockphoto.com/id/127737092/es/v%C3%ADdeo/azul-paro-posici%C3%B3n-elevada-en-la-branch.mp4?s=mp4-640x640-is&k=20&c=87bYgDjA6NhGUts63Nt7lkdsF_nGW_aHKG2zu1SSlMg=">
+      </video>
+      <div>
+        <progress className='progress'
+          max={duration}
+          value={currentTime}
+          onChange={handleProgress}
+        ></progress>
+      </div>
       <div className='nav_controls'>
         <div>
           <button onClick={handleRewind}><FaBackward /></button>
@@ -45,6 +69,9 @@ function App() {
         </div>
         <div>
           <button onClick={handleFast}><FaForward /></button>
+        </div>
+        <div className='duration'>
+          {currentTime.toFixed(2)} | {duration.toFixed(2)}
         </div>
       </div>
     </div>
